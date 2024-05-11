2dc6b34fc27dcd99088baab2e23e6536a35fef42
Paul Momo <mpny98@yahoo.com>
2024-05-11 11:57:29 -0400

    secutity group file created

--- vpc.tf
+++ vpc.tf
@@ -109,61 +109,6 @@ resource "aws_route_table_association" "rta4" {
 
 
 
-
-
-
-resource "aws_security_group" "sg1" {
-    name = "sg1"
-    description = "Allow ssh and httpd"
-    vpc_id = aws_vpc.vpc1.id
-    
-    
-    ingress {
-        description = "allow http"
-        from_port = 80
-        to_port = 80
-        protocol = "tcp"
-        #cidr_blocks = ["0.0.0.0/0"]
-      
-    }
-    egress {
-        from_port = 0
-        to_port = 0
-        protocol = "-1"
-        cidr_blocks = ["0.0.0.0/0"]
-    }
-  tags= {
-    env = "Dev"
-    created-by-terraform = "yes"
-  }
-
-  
-}
-resource "aws_security_group" "sg2" {
-    name = "Terraform-sg-lb"
-    description = "Allow ssh and httpd"
-    vpc_id = aws_vpc.vpc1.id
-    
-    ingress {
-        description = "allow http"
-        from_port = 80
-        to_port = 80
-        protocol = "tcp"
-        cidr_blocks = ["0.0.0.0/0"]
-    }
-    egress {
-        from_port = 0
-        to_port = 0
-        protocol = "-1"
-        cidr_blocks = ["0.0.0.0/0"]
-    }
-  tags= {
-    env = "Dev"
-  } 
-}
-
-
-
 resource "aws_instance" "server1" {
   ami = "ami-02d7fd1c2af6eead0"
   instance_type = "t2.micro"
