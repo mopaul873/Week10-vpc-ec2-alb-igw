24830ac8a8264faa4e8155f52577fe076fd3c2b8
Paul Momo <mpny98@yahoo.com>
2024-05-11 13:01:13 -0400

    updates

--- vpc.tf
+++ vpc.tf
@@ -111,9 +111,9 @@ resource "aws_route_table_association" "rta4" {
 
 resource "aws_instance" "server1" {
   ami = "ami-02d7fd1c2af6eead0"
-  instance_type = "t2.micro"
+  instance_type = var.INSTANCE_TYPE
   vpc_security_group_ids = [ aws_security_group.sg1.id ]
-  availability_zone = "us-east-1a"
+  availability_zone = "$(var.REGION)a"
   subnet_id = aws_subnet.private1.id
   user_data = file("code.sh")
   tags={
@@ -123,9 +123,9 @@ resource "aws_instance" "server1" {
 }
 resource "aws_instance" "server2" {
   ami = "ami-02d7fd1c2af6eead0"
-  instance_type = "t2.micro"
+  instance_type = var.INSTANCE_TYPE
   vpc_security_group_ids = [ aws_security_group.sg1.id ]
-  availability_zone = "us-east-1b"
+  availability_zone = "$(var.REGION)b"
   subnet_id = aws_subnet.private2.id
   user_data = file("code.sh")
   tags={
