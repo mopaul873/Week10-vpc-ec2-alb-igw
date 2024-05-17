6b183ed643787a9dd8aa93996b0958432fef5851
mopaul873 <157943180+mopaul873@users.noreply.github.com>
2024-05-17 12:50:38 -0400

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,14 +1,11 @@
 variable "ENVIRONMENT" {
-default = "dev"
-  
+default = "dev"  
 }
 
 variable "REGION" {
-default = "us-east-1
-  
+default = "us-east-1"  
 }
 
 variable "INSTANCE_TYPE" {
-default = "t2.micro"
-  
+default = "t2.micro"  
 }
